3dbd7537386ed0f2d3f352de2789ce0397bfab25
Dajana781 <dajana.mucic@hotmail.com>
2025-05-22 19:59:52 +0200

    Testni push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.getElementById("forma").addEventListener("submit",  function(event) {
     const password = document.getElementById("sifra").value;
 
     // Privremena validacija (kasnije ćemo spajati s bazom ili API-jem)
-    if (ime === "tetkica" && sifra === "1234") {
+    if (ime === "tekica" && sifra === "1234") {
 //         document.getElementById("message").style.color = "green";
 //         document.getElementById("message").textContent = "Uspješna prijava!";
 //         // Ovjde možeš preusmjeriti na drugu stranicu: window.location.href = "dashboard.html";
